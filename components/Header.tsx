9bd7cda4fab85a536b1e8db699348a6668b26465
Lutfia Aisya <lutfia.aisya@nlc.health>
2024-01-24 17:46:15 +0100

    style: trapezium bg for links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,16 @@
+"use client";
+
 import Link from "next/link";
 import { links } from "@/lib/data";
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
     <header className="z-[999] relative">
-      <div className="fixed top-[6rem] end-[9rem] h-[2rem] max-w-[72rem] rounded-none bg-purple text-violet-100 flex flex-wrap gap-2 sm:flex-nowrap sm:rounded-full text-center">
+      <div className="fixed top-[6rem] end-[9rem] rounded-none text-violet-100 flex flex-wrap gap-2 sm:flex-nowrap sm:rounded-full text-center">
         {links.map((link) => {
           return (
-            <Link  className="w-[10rem] py-1" href={link.hash} key={link.hash}>
+            <Link  className="trapezoid border w-[10rem] py-1" href={link.hash} key={link.hash}>
               {link.name}
             </Link>
           );
