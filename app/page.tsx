880cfccad40b19c2004055ea712048c5d8ff6038
Lutfia Aisya <lutfia.aisya@nlc.health>
2024-01-25 16:45:34 +0100

    style: section background

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
   return (
     <main>
       <Header />
+      <section className="border w-[53rem] h-[37rem] bg-mudgreen -z-50">
+      </section>
       <h1>Lutfia Aisya</h1>
       <h3>Full Stack/Frontend Developer</h3>
       <Intro />
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,10 +7,10 @@ import { motion } from "framer-motion";
 const Header = () => {
   return (
     <header className="z-[999] relative">
-      <div className="fixed top-[6rem] end-[9rem] rounded-none text-violet-100 flex flex-wrap gap-2 sm:flex-nowrap sm:rounded-full text-center">
+      <div className="fixed top-[6rem] end-[9rem] rounded-none text-violet-100 flex items-center justify-center gap-2 sm:flex-nowrap sm:rounded-full text-center">
         {links.map((link) => {
           return (
-            <Link  className="trapezoid border w-[10rem] py-1" href={link.hash} key={link.hash}>
+            <Link  className="trapezoid w-[10rem]" href={link.hash} key={link.hash}>
               {link.name}
             </Link>
           );
