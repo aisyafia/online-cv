ee03156d2d6500ee338311e946f21d5d893ae470
aisyafia <aisya.lutfia@gmail.com>
2025-02-09 20:01:55 +0100

    feat dialog on experience click

--- components/DetailsCard.tsx
+++ components/DetailsCard.tsx
@@ -1,9 +1,43 @@
-const DetailsCard = () => {
-    return (
-        <div>
-            <></>
-        </div>
-    )
-}
+"use client";
 
-export { DetailsCard }
+import {
+  Description,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
+import Image from "next/image";
+import closeIcon from "@/public/close.svg";
+
+const DetailsCard = (props: any) => {
+  return (
+    <Dialog
+      open={props.open}
+      onClose={props.closeDialog}
+      className="relative z-10"
+    >
+      <div className="fixed inset-32 flex w-screen items-center justify-center p-4">
+        <DialogPanel className="max-w-lg relative space-y-4 border bg-mudgreen p-12">
+          <button
+            onClick={props.closeDialog}
+            className="absolute top-2 right-2 z-10 w-fit p-2 bg-transparent text-stone-400 rounded-full"
+          >
+            <Image
+              src={closeIcon}
+              alt="close"
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+          </button>
+          <DialogTitle className="font-bold"></DialogTitle>
+          <Description className="text-base font-light bg-mudgreen">
+            {props.description}
+          </Description>
+        </DialogPanel>
+      </div>
+    </Dialog>
+  );
+};
+
+export { DetailsCard };
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -4,18 +4,22 @@ import { experiencesData } from "@/lib/data";
 import { Motion } from "./Motion";
 import { useState } from "react";
 import { Prompt } from "next/font/google";
+import { DetailsCard } from "./DetailsCard";
 
 const prompt = Prompt({ subsets: ["latin"], weight: ["300"] });
 
 const Experience = () => {
-  const [open, setOpen] = useState(false);
+  const [openExp, setOpenExp] = useState<string | null>(null);
+
   return (
     <section className="scroll-mt-16 max-w-[50rem] grid grid-cols-2 gap-4">
       {experiencesData.map((exp) => {
+        const isOpen = openExp === exp.title;
+
         return (
           <button
             key={exp.title}
-            onClick={() => setOpen(!open)}
+            onClick={() => setOpenExp(isOpen ? null : exp.title)}
             className="w-full text-left p-1 border border-gray-300 rounded-lg shadow-sm hover:shadow-md hover:shadow-neutral-300 transition"
           >
             <Motion
@@ -38,9 +42,14 @@ const Experience = () => {
               <div>
                 <h5 className="text-base font-light">{exp.date}</h5>
               </div>
-
-              <p className="text-left">{exp.description}</p>
             </Motion>
+            {isOpen && (
+              <DetailsCard
+                open={isOpen}
+                closeDialog={() => setOpenExp(null)}
+                description={exp.description}
+              />
+            )}
           </button>
         );
       })}
